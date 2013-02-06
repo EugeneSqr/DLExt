47a47186574355dd7181d6ce23277de27cdf54ae
Nikolay Anikin <an2114@mail.ru>
2013-02-06 13:30:10 +0400

    script files added

---
+++ DLExt.WebApplication/Scripts/LoadXmlDoc.js
@@ -0,0 +1,116 @@
+﻿function onPageLoad() {
+    doAjaxCall('http://dlext.ru/Service.svc/GetLocations', 'GET', function (json) {
+        fillLocationsList(json);
+    });
+    doAjaxCall('http://dlext.ru/Service.svc/GetAllPersons', 'GET', function (json) {
+        fillPersonsDropDownList(json);
+    });
+}
+
+function doAjaxCall(url, method, onSuccess) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest;
+    } else { // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var response = xmlhttp.response;
+            var json = JSON.parse(response);
+            onSuccess(json);
+        } else {
+            if (xmlhttp.readyState == 4) alert('error');
+        }
+    };
+    xmlhttp.open(method, url, true);
+    xmlhttp.send();
+}
+
+function doAjaxCallPost(url, method, params, onSuccess) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest;
+    } else { // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var response = xmlhttp.response;
+            var json = JSON.parse(response);
+            onSuccess(json);
+        } else {
+            if (xmlhttp.readyState == 4) alert('error');
+        }
+    };
+    xmlhttp.open(method, url, true);
+    xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xmlhttp.send(params);
+}
+
+function fillLocationsList(locations) {
+    var div = document.getElementById("checkBoxListLocations");
+    for (var i = 0; i < locations.length; i++) {
+        var checkbox = document.createElement('input');
+        checkbox.id = "checkBoxId" + i;
+        checkbox.type = "checkbox";
+        checkbox.name = locations[i].Name;
+        checkbox.value = JSON.stringify(locations[i]);
+        checkbox.onchange = onCheckBoxChange;
+        //checkbox.addEventListener('onchange', function () { getJsonSelectedLocation(); }, false);
+
+        var label = document.createElement('label');
+        label.htmlFor = checkbox.id;
+        label.appendChild(document.createTextNode(locations[i].Name));
+        label.appendChild(document.createElement('br'));
+
+        div.appendChild(checkbox);
+        div.appendChild(label);
+    }
+}
+
+function fillPersonsDropDownList(persons) {
+    var dropDownList = document.getElementById("personsDropDownList");
+    for (var i = 0; i < persons.length; i++) {
+        var option = document.createElement('option');
+        option.value = persons[i].Email;
+
+        option.appendChild(document.createTextNode(persons[i].DisplayName));
+        dropDownList.appendChild(option);
+    }
+}
+
+function onCheckBoxChange() {
+    var request = encodeURIComponent(buildFilterRequestString(getJsonSelectedLocation()));
+//    doAjaxCall('http://dlext.ru/Service.svc/GetPersons?Request=' + request, 'GET', function(json) {
+//        fillPersonsDropDownList(json);
+    doAjaxCallPost('http://dlext.ru/Service.svc/GetPersonsPost', 'POST', request, function (json) {
+        fillPersonsDropDownList(json);
+    });
+}
+
+//function getJsonSelectedLocation() {
+//    var div = document.getElementById("checkBoxListLocations");
+//    var childs = div.childNodes;
+//    var selectedLocations = [];
+//    for (var i = 0; i < childs.length; i++) {
+//        if (childs[i].type == "checkbox") {
+//            if (childs[i].checked)
+//                selectedLocations.push(childs[i].value);
+//        }
+//    }
+//    return selectedLocations;
+//}
+
+function buildFilterRequestString(locations, persons) {
+    var filterRequest = {};
+    if (locations == undefined)
+        locations = [];
+    if (persons == undefined)
+        persons = [];
+    filterRequest.ExcludedPersons = persons;
+    filterRequest.Locations = locations;
+    return JSON.stringify(filterRequest);
+}
---
+++ DLExt.WebApplication/Scripts/default.js
@@ -0,0 +1,183 @@
+﻿var excludedPersonIndexes = [];
+var persons = [];
+var locations = [];
+
+$(document).ready(function () {
+
+    $('#clearButton').click(function () {
+        excludedPersonIndexes = [];
+
+        onPropertyChanged();
+    });
+
+    $('#excludeButton').click(function () {
+        var personId = parseInt($('#personsDropDownList').val());
+        if ($.inArray(personId, excludedPersonIndexes) >= 0)
+            return;
+        excludedPersonIndexes.push(personId);
+
+        onPropertyChanged();
+    });
+
+    $('#sendLetter').click(function () {
+        return ($(this).attr('disabled')) ? false : true;
+    });
+
+    $.ajax({
+        url: 'http://dlext.ru/Service.svc/GetGroupedPersons',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            buildPersonsList(data);
+
+            $(locations).each(function () {
+                var checkbox = document.createElement("input");
+                checkbox.id = 'checkBoxLocation' + this.Name;
+                checkbox.type = "checkbox";
+                checkbox.name = this.Name;
+                $(checkbox).change(function (e) {
+                    var location = getLocationByName(checkbox.name);
+                    location.IsSelected = e.target.checked;
+                    updatePersonsList();
+                    onPropertyChanged();
+                });
+
+                $(checkbox).appendTo($('#checkBoxListLocations'));
+                $('#checkBoxListLocations').append(this.Name + '<br/>');
+            });
+            var dropDownList = $('#personsDropDownList');
+            $(persons).each(function () {
+                var option = document.createElement('option');
+                option.value = this.Index;
+                option.appendChild(document.createTextNode(this.Name));
+                dropDownList.append(option);
+            });
+
+            update();
+        }
+    });
+});
+
+function buildPersonsList(rawData) {
+    var index = 0;
+    persons = [];
+    $(rawData).each(function (ind1, keyValuePair) {
+        var location = keyValuePair.Key.Name;
+        locations.push({
+            "Name": location,
+            "IsSelected": false
+        });
+        $(keyValuePair.Value).each(function (ind2, person) {
+            persons.push({
+                "Index": index,
+                "Name": person.DisplayName,
+                "Email": person.Email,
+                "Location": location,
+                "IsSelected": false
+            });
+            index++;
+        });
+    });
+}
+
+function onPropertyChanged() {
+    var email = 'mailto:';
+
+    var personsInList = [];
+    $(persons).each(function () {
+        if (getLocationByName(this.Location).IsSelected) {
+            if ($.inArray(this.Index, excludedPersonIndexes) == -1)
+                personsInList.push(this);
+        }
+    });
+    $('#personsCountLabel').html(personsInList.length);
+
+    $(personsInList).each(function () {
+        email += this.Email + '; ';
+    });
+
+    if (personsInList.length == 0) {
+        $('#sendLetter').attr('disabled', true);
+        $('#sendLetter').attr('href', '');
+    } else {
+        $('#sendLetter').attr('disabled', false);
+        $('#sendLetter').attr('href', email);
+    }
+
+    var personList = $('#excludedPersons');
+    personList.empty();
+    $(excludedPersonIndexes).each(function (i, value) {
+        var person = getPersonByIndex(value);
+        var div = $('<div></div>');
+        div.attr('id', 'personSelector' + i);
+        div.append($('<label>' + person.Name + '</label>'));
+
+        var hyperlink = document.createElement("a");
+        hyperlink.href = '';
+        hyperlink.textContent = 'удалить';
+        $(hyperlink).click(function (e) {
+            e.preventDefault();
+            excludedPersonIndexes = $.grep(excludedPersonIndexes, function (value1) {
+                return value1 != value;
+            });
+            $('#personSelector' + i).remove();
+            
+            onPropertyChanged();
+        });
+        div.append($(hyperlink));
+        div.append($('<br/>'));
+
+        div.appendTo(personList);
+
+    });
+}
+
+function update() {
+    var count = 0;
+    $(persons).each(function () {
+        if (this.IsSelected)
+            count++;
+    });
+    $('#personsCountLabel').html(count);
+}
+
+function updatePersonsList() {
+    var dropDownList = $('#personsDropDownList');
+    dropDownList.empty();
+    $(persons).each(function () {
+        if (getLocationByName(this.Location).IsSelected) {
+            var option = document.createElement('option');
+            option.value = this.Index;
+            option.appendChild(document.createTextNode(this.Name));
+            dropDownList.append(option);
+        }
+    });
+}
+
+function getLocationByName(name) {
+    var result = $.grep(locations, function (item) {
+        return item.Name == name;
+    });
+    if (result.length != 1)
+        alert("can't find an element");
+    return result[0];
+}
+
+function getPersonByIndex(index) {
+    var result = $.grep(persons, function (item) {
+        return item.Index == index;
+    });
+    if (result.length != 1)
+        alert("can't find an element");
+    return result[0];
+}
+
+function getLocationPersonsCount(locationName) {
+    var count = 0;
+    $(persons).each(function () {
+        if (this.Location == locationName)
+            count++;
+    });
+    return count;
+}
+
