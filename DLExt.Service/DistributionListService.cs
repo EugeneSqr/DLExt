cfeaa049a73241fd408c299c3d3c63681e2d8b6f
Evgeny Skurikhin <eskurihin@gmail.com>
2013-02-22 08:34:28 +0400

    ASP.net caching has been added

--- DLExt.Service/DistributionListService.cs
+++ DLExt.Service/DistributionListService.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
 using DLExt.Infrastructure;
 using DLExt.Model;
 
 namespace DLExt.Service
 {
+    [AspNetCompatibilityRequirements(RequirementsMode=AspNetCompatibilityRequirementsMode.Allowed)]
     public class DistributionListService : IDistributionListService
     {
         private readonly Repository repository;
@@ -15,12 +17,14 @@ namespace DLExt.Service
         }
 
         [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        [AspNetCacheProfile("CacheFor120Seconds")]
         public IList<Person> GetPersons()
         {
             return repository.GetPersons();
         }
 
         [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        [AspNetCacheProfile("CacheFor120Seconds")]
         public IList<string> GetLocations()
         {
             return repository.GetLocations();
